6710a2034a227d8fb9df21a5071be651e8da9e05
rkdejr2321 <rkdejr2321@naver.com>
2023-03-13 13:54:21 +0900

    [Fatch] 화면 구성 수정

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,72 +1,87 @@
-import './css/pages.css';
+import './css/team.css';
 
-import pjs from '../images/박재석.png'
-import pdy from '../images/박도영.png'
-import skd from '../images/서강덕.png'
+import pjs from '../images/박재석.jpg'
+import pdy from '../images/박도영.jpg'
+import skd from '../images/서강덕.jpg'
 import ahb from '../images/안해빈.jpg'
 import sjw from '../images/서지원.jpg'
 
-const Team = () =>{
+const Team = () => {
     return (
-        <div className='body'>
-            <p className='sub_title'>팀원</p>
-            <table align="center">
+        <div className='container'>
+            <p className='sub_title1'>팀원</p>
 
-                <thead align="center">
-                    <tr>
-                        <th width="300">Image</th>
-                        <th width="100">Name</th>
-                        <th width="300">Department</th>
-                        <th width="300">Email</th>
-                        <th width="300">Phone</th>
-                        <th width="300">Github</th>
-                    </tr>
-                </thead>
-                <tbody align="center">
-                    <tr>
-                        <td><img src={pdy} alt="박도영" /></td>
-                        <td>박도영</td>
-                        <td>AI·소프트웨어학부(소프트웨어전공)</td>
-                        <td>doodoo55@gachon.ac.kr</td>
-                        <td>010-6633-3653</td>
-                        <td>https://github.com/Doyoung-Park</td>
-                    </tr>
-                    <tr>
-                        <td><img src={pjs} alt="박재석" /></td>
-                        <td>박재석</td>
-                        <td>산업경영공학과</td>
-                        <td>qkrwotjr0@gmail.com</td>
-                        <td>010-8612-8138</td>
-                        <td>https://github.com/Jedo0224</td>
-                    </tr>
-                    <tr>
-                        <td><img src={skd} alt="서강덕" /></td>
-                        <td>서강덕</td>
-                        <td>컴퓨터공학과</td>
-                        <td>rkdejr2321@naver.com</td>
-                        <td>010-8731-2312</td>
-                        <td>https://github.com/rkdejr2321</td>
-                    </tr>
-                    <tr>
-                        <td><img src={sjw} alt="서지원" /></td>
-                        <td>서지원</td>
-                        <td>AI·소프트웨어학부(소프트웨어전공)</td>
-                        <td>jwjw6410@gmail.com</td>
-                        <td>010-4725-8654</td>
-                        <td>https://github.com/manips1</td>
-                    </tr>
-                    <tr>
-                        <td><img src={ahb} alt="안해빈" /></td>
-                        <td>안해빈</td>
-                        <td>AI·소프트웨어학부(소프트웨어전공)</td>
-                        <td>gnh06280@naver.com</td>
-                        <td>010-9870-3043</td>
-                        <td>https://github.com/HaebinAHN</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className='card-box'>
+                <div className="card-list">
+                    <div className="card">
+                        <h3>박도영</h3>
+                        <h3>PARK DO YOUNG</h3>
+                        <img src={pdy} alt="doyoung_img" />
+                        <div align="center" className='member-role'>LEADER</div>
+                        <p className='member-info'>🧑‍💻 Software Engineering</p>
+                        <p>📱 010-6633-3653 </p>
+                        <p>📧 doodoo55@gachon.ac.kr </p>
+                        <a href='https://github.com/Doyoung-Park' target="_balank">
+                            <img id='badge' src="https://img.shields.io/badge/github-181717?style=for-the-badge&logo=github&logoColor=white"/> 
+                        </a>
+                    </div>
+                    <div className="card">
+                        <h3>박재석</h3>
+                        <h3>PARK JAE SEOK</h3>
+                        <img src={pjs} alt="Placeholder Image" />
+                        <div align="center" className='member-role'>MEMEBER</div>
+                        <p className='member-info'>🧑‍💻 Software Engineering</p>
+                        <p>📱 010-8612-8138 </p>
+                        <p>📧 qkrwotjr0@gmail.com</p>
+                        <a href='https://github.com/Jedo0224' target="_balank">
+                            <img id='badge' src="https://img.shields.io/badge/github-181717?style=for-the-badge&logo=github&logoColor=white"/> 
+                        </a>
+                    </div>
+                </div>
+
+                <div className="card-list">
+                    <div className="card">
+                        <h3>서강덕</h3>
+                        <h3>SEO KANG DEOK</h3>
+                        <img src={skd} alt="Placeholder Image" />
+                        <div align="center" className='member-role'>MEMBER</div>
+                        <p className='member-info'>🧑‍💻 Computer Engineering</p>
+                        <p>📱 010-8731-2312</p>
+                        <p>📧 rkdejr2321@naver.com </p>
+                        <a href='https://github.com/rkdejr2321' target="_balank">
+                            <img id='badge' src="https://img.shields.io/badge/github-181717?style=for-the-badge&logo=github&logoColor=white"/>
+                        </a>
+                    </div>
+
+                    <div className="card">
+                        <h3>서지원</h3>
+                        <h3>SEO JI ONE</h3>
+                        <img src={sjw} alt="Placeholder Image" />
+                        <div align="center" className='member-role'>MEMBER</div>
+                        <p className='member-info'>🧑‍💻 Software Engineering</p>
+                        <p>📱 010-4725-8654 </p>
+                        <p>📧 jwjw6410@gmail.com </p>
+                        <a href='https://github.com/manips1' target="_balank">
+                            <img id='badge' src="https://img.shields.io/badge/github-181717?style=for-the-badge&logo=github&logoColor=white"/> 
+                        </a>
+                    </div>
+
+                    <div className="card">
+                        <h3>안해빈</h3>
+                        <h3>AN HAE BIN</h3>
+                        <img src={ahb} alt="Placeholder Image" />
+                        <div align="center" className='member-role'>MEMBER</div>
+                        <p className='member-info'>🧑‍💻 Software Engineering</p>
+                        <p>📱 010-9870-3043 </p>
+                        <p>📧 gnh06280@naver.com</p>
+                        <a href='https://github.com/HaebinAHN ' target="_balank">
+                            <img id='badge' src="https://img.shields.io/badge/github-181717?style=for-the-badge&logo=github&logoColor=white"/> 
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Team;
