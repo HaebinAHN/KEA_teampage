8e7202d6454ae59aedce89d1e95abf06db6e9366
Doyoung <doodoo55@gachon.ac.kr>
2023-03-16 12:15:18 +0900

    [feat] 객관식, 주관식 페이지 추가

--- src/pages/Survey/MultipleChoice.jsx
+++ src/pages/Survey/MultipleChoice.jsx
@@ -1,11 +1,44 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
-import TextInput from "../ui/TextInput";
+import { Grid, TextField } from "@mui/material";
+function MultipleChoice(props) {
+    const [options, setOptions] = useState([]);
 
-function MultipleChoice(props){
-    return(
-        <>Multi</>
+    function addOption() {
+        setOptions([...options, { text: '' }]);
+    }
+
+    function handleChangeOptionText(index, event) {
+        const newOptions = [...options];
+        newOptions[index].text = event.target.value;
+        setOptions(newOptions);
+    }
+
+    function renderOptions() {
+        return options.map((option, index) => (
+            <div key={index}>
+                <label>
+                    Option {index + 1}:{' '}
+                    <input
+                        type="text"
+                        value={option.text}
+                        onChange={(event) => handleChangeOptionText(index, event)}
+                    />
+                </label>
+            </div>
+        ));
+    }
+
+    return (
+        <div>
+            <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
+                질문: <TextField placeholder="내용을 입력하세요" fullWidth />
+            </Grid>
+            <br />
+            <div>
+                <button onClick={addOption}>Add Option</button>
+            </div>
+            {renderOptions()}
+        </div>
     );
 }
 export default MultipleChoice;
--- src/pages/Survey/ShortAnswer.jsx
+++ src/pages/Survey/ShortAnswer.jsx
@@ -2,10 +2,39 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import TextInput from "../ui/TextInput";
+import { Button, Grid, TextField } from "@mui/material";
+function ShortAnswer(props) {
 
-function ShortAnswer(props){
-    return(
-        <>short</>
+    const [item, setItem] = useState({ title: '' });
+    const addItem = props.addItem;
+
+    const onButtonClick = () => {
+        addItem(item);
+        setItem({ title: "" });
+    };
+
+    const onInputChange = (e) => {
+        setItem({ title: e.target.value });
+        console.log(item);
+    };
+
+
+    return (
+        <>
+            <Grid container style={{ marginTop: 20 }}>
+                <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
+                    질문: <TextField placeholder="질문을 입력하세요" fullWidth onChange={onInputChange} value={item.title} />
+                </Grid>
+                <Grid xs={1} md={1} item>
+                    <Button fullWidth style={{ height: '100%' }} color="secondary" variant="outlined" onClick={onButtonClick}>
+                        +
+                    </Button>
+
+                </Grid>
+
+            </Grid>
+
+        </>
     );
 }
 export default ShortAnswer;
